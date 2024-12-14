27f55ff3e1e6644b5809f3329103b6303cc54fcf
Row <reardenr@gmail.com>
2024-12-14 00:17:50 -0300

    Add prune to config setup. Only enable txindex when not pruned.

--- bitcoind/setup.sh
+++ bitcoind/setup.sh
@@ -4,8 +4,16 @@
 
 prepare "bitcoin.conf"
 
-save_config "datadir" "/data"
+save_config "Data directory" "/data"
 prompt_config "Chain (main|test|signet|regtest)" "chain" "test"
-prompt_config "Enable debug?" "debug" "1"
+prompt_config "Enable debug" "debug" "1"
+
+PRUNE=$(ask "Prune (size in MB)" "no")
+
+if [ "$PRUNE" != "no" ]; then
+    save_config "prune" $PRUNE
+else
+    save_config "txindex" "1"
+fi
 
 finalize
